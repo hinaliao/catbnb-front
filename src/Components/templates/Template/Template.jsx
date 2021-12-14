13695c42cdc1610338fb28e4ffc3cdf0dca523be
Hina Liao <hinah.zh@gmail.com>
2021-12-14 20:34:32 -0300

    Remove h1 element

--- src/Components/templates/Template/Template.jsx
+++ src/Components/templates/Template/Template.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 
 const Template = ({ children }) => (
   <div className="template-container">
-    <h1>CatBnb</h1>
     <div className="template-content">
       {children}
     </div>
